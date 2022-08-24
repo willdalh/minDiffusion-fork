e0f4cc5684f930c502f356a49827cfff3c747ebd
https://gitlab.stud.iie/williad <williad@stud.ntnu.no>
2022-08-24 09:09:24 +0200

    num workers

--- superminddpm.py
+++ superminddpm.py
@@ -153,7 +153,7 @@ def train_mnist(n_epoch: int = 100, device= "cuda:0" if torch.cuda.is_available(
         download=True,
         transform=tf,
     )
-    dataloader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=8)
+    dataloader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=mp.cpu_count())
     optim = torch.optim.Adam(ddpm.parameters(), lr=2e-4)
 
     for i in range(n_epoch):
