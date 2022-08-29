63f352fb105512cb2ea408ddb926cf06c9c43ee7
https://gitlab.stud.iie/williad <williad@stud.ntnu.no>
2022-08-29 13:49:56 +0200

    change

--- sample_using_concept.py
+++ sample_using_concept.py
@@ -15,14 +15,15 @@ def main():
     print(concept_vector.shape)
     
     n_samples = 10
-    for i in range(n_samples):
-        factor = (i/n_samples) + 0.5
-        starting_noise = (factor * concept_vector).reshape(1, 28, 28)[:, None, ...]
-        # print(starting_noise.shape)
-        sampled = model.sample(1, (1, 28, 28), starting_noise=starting_noise, device=device)
-        print(f"Finished {i}/{n_samples}")
-        sampled = sampled + 0.5
-        save_image(sampled, f"./concept_results/result_{factor}.png")
+    with torch.no_grad():
+        for i in range(n_samples):
+            factor = (i/n_samples) + 0.5
+            starting_noise = (factor * concept_vector).reshape(1, 28, 28)[:, None, ...]
+            # print(starting_noise.shape)
+            sampled = model.sample(1, (1, 28, 28), starting_noise=starting_noise, device=device)
+            print(f"Finished {i}/{n_samples}")
+            sampled = sampled + 0.5
+            save_image(sampled, f"./concept_results/result_{factor}.png")
 
 if __name__ == "__main__":
     main()
