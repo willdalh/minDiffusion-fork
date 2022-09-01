512f7ab3f319a1f12999c8053b8cd4e388e8af99
https://gitlab.stud.iie/williad <williad@stud.ntnu.no>
2022-09-01 14:00:09 +0200

    work

--- sampler.py
+++ sampler.py
@@ -12,16 +12,21 @@ def main():
     # sampled = model.sample(1, (1, 28, 28), device)
     # print(sampled.shape)
 
-    n = 100
+    n = 900
     dataset = torch.Tensor(n, 2, 28, 28)
-
+    seed = 307
+    torch.manual_seed(seed)
     with torch.no_grad():
-        sampled, original_noise = model.sample(n, (1, 28, 28), device, return_original_noise=True)
-        dataset[:, 0] = sampled[:, 0]
-        dataset[:, 1] = original_noise[:, 0]
+        for i in range(n):
+            sampled, original_noise = model.sample(1, (1, 28, 28), device, return_original_noise=True)
+            dataset[i, 0] = sampled[0, 0]
+            dataset[i, 1] = original_noise[0, 0]
+        # dataset[:, 0] = sampled[:, 0]
+        # dataset[:, 1] = original_noise[:, 0]
     # sampled, original_noise = torch.zeros(n, 1, 28, 28).to(device), torch.ones(n, 1, 28, 28).to(device)
 
     torch.save(dataset, f"./datasets/{n}_samples.pth")
+    torch.save(torch.LongTensor([seed]), f"./datasets/{n}_seed.pth")
     # dataset[:, 1]
 
     # print("dataset shape", dataset.shape)
