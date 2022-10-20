4d4e790efc846c90b9ace7ac6de805ad90a47ea2
https://gitlab.stud.iie/williad <williad@stud.ntnu.no>
2022-10-20 09:11:47 +0200

    sampler can use colors

--- sampler.py
+++ sampler.py
@@ -5,9 +5,11 @@ from superminddpm import DDPM, DummyEpsModel
 import torch
 
 def main():
+    use_colors = True
+
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    model = DDPM(eps_model=DummyEpsModel(1), betas=(1e-4, 0.02), n_T=1000)
-    model.load_state_dict(torch.load("./contents/ddpm_mnist.pth", map_location=device))
+    model = DDPM(eps_model=DummyEpsModel(3 if use_colors else 1), betas=(1e-4, 0.02), n_T=1000)
+    model.load_state_dict(torch.load("./contents/colors2/ddpm_mnist_colors.pth", map_location=device))
     model.to(device)
     model.eval()
     
@@ -15,24 +17,28 @@ def main():
     # sampled = model.sample(1, (1, 28, 28), device)
     # print(sampled.shape)
 
-    n = 980
-    dataset = torch.Tensor(n, 2, 28, 28)
+    n = 1000
+    dataset = torch.Tensor(n, 2, 3, 28, 28) if use_colors else torch.Tensor(n, 2, 28, 28) 
     # seeds = torch.randint(0, 10000000, (n,), dtype=torch.long)
 
     seed = torch.LongTensor([980])
     torch.manual_seed(seed)
     with torch.no_grad():
         # for i in range(n):
-        sampled, original_noise = model.sample(n, (1, 28, 28), device, return_original_noise=True)
-        dataset[:, 0] = sampled[:, 0]
-        dataset[:, 1] = original_noise[:, 0]
+        sampled, original_noise = model.sample(n, (3 if use_colors else 1, 28, 28), device, return_original_noise=True)
+        dataset[:, 0] = sampled if use_colors else sampled[:, 0]
+        dataset[:, 1] = original_noise if use_colors else original_noise[:, 0]
             # if i % 40 == 0:
                 # print(f"Currently at {i} of {n}")
         # dataset[:, 0] = sampled[:, 0]
         # dataset[:, 1] = original_noise[:, 0]
 
-    torch.save(dataset, f"./datasets/{n}_samples.pth")
-    torch.save(seed, f"./datasets/{n}_seed.pth")
+    if use_colors:
+        torch.save(dataset, f"./datasets/colors/{n}_samples.pth")
+        torch.save(seed, f"./datasets/colors/{n}_seed.pth")
+    else:   
+        torch.save(dataset, f"./datasets/{n}_samples.pth")
+        torch.save(seed, f"./datasets/{n}_seed.pth")
     # dataset[:, 1]
 
     # print("dataset shape", dataset.shape)
