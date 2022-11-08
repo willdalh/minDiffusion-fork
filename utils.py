5ca778079d7ff9d6f9105304f5c110c12c096b15
William Dalheim <williad@stud.ntnu.no>
2022-11-08 12:41:39 +0100

    ddim samples not working oooops

--- utils.py
+++ utils.py
@@ -1,10 +1,17 @@
 import matplotlib.pyplot as plt
 
-def plot_samples(samples, title=None, normalize=False):
-    to_plot = samples;
 
-    if normalize:
-        to_plot = (to_plot - to_plot.min()) / (to_plot.max() - to_plot.min())
+# * FOR VISUALIZATION
+
+def plot_samples(images, title=None, norm=False, norm_across_batch=False, clip=False):
+    if norm and clip:
+        raise ValueError("Cannot normalize and clip at the same time.")
+    
+    to_plot = images.detach().clone();
+    if norm:
+         to_plot = normalize(to_plot, norm_across_batch)
+    elif clip:
+        to_plot = to_plot.clamp(0, 1)
 
     n = to_plot.shape[0]
     fig, axs = plt.subplots(1, n, figsize=(n * 3, 3))
@@ -19,3 +26,23 @@ def plot_samples(samples, title=None, normalize=False):
             curr_axs.imshow(to_plot[i].cpu())
            
     plt.show()
+           
+    plt.show()
+
+def normalize(x, across_batch=False):
+    x_copy = x.detach().clone()
+    out = None
+    if across_batch:
+        out = _normalize_across_all_dims(x_copy)
+    else:
+        for i in range(x_copy.shape[0]):
+            x_copy[i] = _normalize_across_all_dims(x_copy[i])
+        out = x_copy
+    return out
+        
+
+def _normalize_across_all_dims(x):
+    inv_scale = x.max() - x.min()
+    if inv_scale == 0:
+        inv_scale = 1
+    return (x - x.min()) / (inv_scale)
